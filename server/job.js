84fa1e02611cff6ffd699e470bb4c31f39aa41b5
Nadee Anuththara <nadee013@gmail.com>
2014-12-08 10:11:07 +0530

    fix server revised

--- server/job.js
+++ server/job.js
@@ -26,28 +26,33 @@ Meteor.methods({
     return Jobs.insert(doc);
   },
 
-  'assignJobToShift': function(jobId, shiftId) {
-    if(jobId) {
-      var job = Jobs.findOne(jobId);
-      if(job) {
-        if(job.onshift) {
-          Shifts.update({_id: job.onshift}, {$pull: {"jobs": jobId}});
-        }
-        //if shift Id exists job move to that shift
-        //if does not remove from the shift
-        if(shiftId) {
-          var shift = Shifts.findOne(shiftId);
-          if(shift) {
-            Shifts.update({_id: shiftId}, {$addToSet: {"jobs": jobId}});
-            Jobs.update({_id: jobId}, {$set: {"onshift": shiftId}});
-          }
-        } else {
-          Shifts.update({_id: shiftId}, {$pull: {"jobs": jobId}});
-          Jobs.update({_id: jobId}, {$set: {"onshift": null}});
-        } 
-      } else {
-        throw new Meteor.Error(404, "Job id field not found");
-      } 
+  'assignJobToShift': function(jobId, shiftId, jobStartTime) {
+    if(!jobId) {
+      throw new Meteor.Error(404, "Job id field not found");
     }
-  },
+    var job = Jobs.findOne(jobId);
+    if(!job) {
+      throw new Meteor.Error(404, "Job not found");
+    }
+    var jobDoc = {};
+    jobDoc.job = jobId;
+    if(job.onshift) {
+      console.log("Job already on a shift, remove from shift", job.onshift, jobId);
+      Shifts.update({_id: job.onshift}, {$pull: {"jobs": jobDoc}});
+    }
+    //if shift Id exists job move to that shift
+    //if does not remove from the shift
+    if(shiftId) {
+      var shift = Shifts.findOne(shiftId);
+      if(!shift) {
+        throw new Meteor.Error(404, "Shift not found");
+      }
+      if(jobStartTime) {
+        jobDoc.start = jobStartTime;
+      }
+      console.log("Job set to the shift", shiftId, jobId);
+      Shifts.update({_id: shiftId}, {$addToSet: {"jobs": jobDoc}});
+      Jobs.update({_id: jobId}, {$set: {"onshift": shiftId}});
+    }
+  }
 });
--- server/worker.js
+++ server/worker.js
@@ -23,34 +23,38 @@ Meteor.methods({
     if(!worker) {
       throw new Meteor.Error(404, "Worker not found");
     }
-    if(shiftId) {
-      var shift = Shifts.findOne(shiftId);
-      if(!shift) {
-        throw new Meteor.Error(404, "Shift not found");
-      }
-      Shifts.update({_id: shiftId}, {$set: {"assignedTo": workerId}});
-      Workers.update({_id: workerId}, {$set: {"availability": false}});// need algorithm
-    } else {
-      Workers.update({_id: workerId}, {$set: {"availability": true}});
+    if(!shiftId) {
+      Workers.update({_id: workerId}, {$set: {"availability": true}});//need an algo
     }
+    var shift = Shifts.findOne(shiftId);
+    if(!shift) {
+      throw new Meteor.Error(404, "Shift not found");
+    }
+    if(shift.assignedTo) {
+      throw new Meteor.Error(404, "Shift has already been assigned");
+    }
+    console.log("Shift been assigned to a new worker", shiftId, workerId);
+    Shifts.update({_id: shiftId}, {$set: {"assignedTo": workerId}});
+    Workers.update({_id: workerId}, {$set: {"availability": false}});// need algorithm
   },
 
-  'deleteWorkerAssignedShift': function(workerId, shiftId) {
-    if(workerId) {
-      var worker = Workers.findOne(workerId);
-      if(worker) {
-        if(shiftId) {
-          var shift = Shifts.findOne(shiftId);
-          if(shift) {
-            Shifts.update({_id: shiftId}, {$pull: {"assignedTo": workerId}});
-            Workers.update({_id: workerId}, {$set: {"availability": true}, $pull: {"assignedShifts": shiftId}});
-          }
-        } else {
-          Workers.update({_id: workerId}, {$set: {"availability": true}});
-        }
-      }
-    } else {
+  'deleteWorkerFromAssignedShift': function(workerId, shiftId) {
+    if(!workerId) {
       throw new Meteor.Error(404, "Worker id field not found");
     }
+    var worker = Workers.findOne(workerId);
+    if(!worker) {
+      throw new Meteor.Error(404, "Worker----- not found");
+    }
+    if(!shiftId) {
+      throw new Meteor.Error(404, "Shift id field not found");
+    }
+    var shift = Shifts.findOne(shiftId);
+    if(!shift) {
+      throw new Meteor.Error(404, "Shift not found");
+    }
+    console.log("Shift assigned worker has been removed", shiftId, workerId);
+    Shifts.update({_id: shiftId}, {$set: {"assignedTo": null}});
+    Workers.update({_id: workerId}, {$set: {"availability": true}});
   }
 });
