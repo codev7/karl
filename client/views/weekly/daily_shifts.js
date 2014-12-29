f69a8738dc6d718176692f47157a08eb88e27d4f
Nadee Anuththara <nadee013@gmail.com>
2014-12-29 11:38:37 +0530

    fixed day of week

--- client/views/weekly/daily_shifts.js
+++ client/views/weekly/daily_shifts.js
@@ -36,12 +36,51 @@ function getDaysOfWeek(date) {
   var day6 = day1 + 5; // last day is the first day + 6
   var day7 = day1 + 6; // last day is the first day + 6
 
-  var day1ofWeek = new Date(curr.setDate(day1)).toISOString().slice(0,10).replace(/-/g,"-");
-  var day2ofWeek = new Date(curr.setDate(day2)).toISOString().slice(0,10).replace(/-/g,"-");
-  var day3ofWeek = new Date(curr.setDate(day3)).toISOString().slice(0,10).replace(/-/g,"-");
-  var day4ofWeek = new Date(curr.setDate(day4)).toISOString().slice(0,10).replace(/-/g,"-");
-  var day5ofWeek = new Date(curr.setDate(day5)).toISOString().slice(0,10).replace(/-/g,"-");
-  var day6ofWeek = new Date(curr.setDate(day6)).toISOString().slice(0,10).replace(/-/g,"-");  
-  var day7ofWeek = new Date(curr.setDate(day7)).toISOString().slice(0,10).replace(/-/g,"-");
-  return [day1ofWeek, day2ofWeek, day3ofWeek, day4ofWeek, day5ofWeek, day6ofWeek, day7ofWeek]
+  var weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+
+  var date1ofWeek = new Date(curr.setDate(day1));
+  var date2ofWeek = new Date(curr.setDate(day2));
+  var date3ofWeek = new Date(curr.setDate(day3));
+  var date4ofWeek = new Date(curr.setDate(day4));
+  var date5ofWeek = new Date(curr.setDate(day5));
+  var date6ofWeek = new Date(curr.setDate(day6));  
+  var date7ofWeek = new Date(curr.setDate(day7));
+  var doc = [
+    {
+      "date": date1ofWeek.toISOString().slice(0,10).replace(/-/g,"-"),
+      "day": weekday[date1ofWeek.getDay()]
+    },
+    {
+      "date": date2ofWeek.toISOString().slice(0,10).replace(/-/g,"-"),
+      "day": weekday[date2ofWeek.getDay()]
+    },
+    {
+      "date": date3ofWeek.toISOString().slice(0,10).replace(/-/g,"-"),
+      "day": weekday[date3ofWeek.getDay()]
+    },
+    {
+      "date": date4ofWeek.toISOString().slice(0,10).replace(/-/g,"-"),
+      "day": weekday[date4ofWeek.getDay()]
+    },
+    {
+      "date": date5ofWeek.toISOString().slice(0,10).replace(/-/g,"-"),
+      "day": weekday[date5ofWeek.getDay()]
+    },
+    {
+      "date": date6ofWeek.toISOString().slice(0,10).replace(/-/g,"-"),
+      "day": weekday[date6ofWeek.getDay()]
+    },
+    {
+      "date": date7ofWeek.toISOString().slice(0,10).replace(/-/g,"-"),
+      "day": weekday[date7ofWeek.getDay()]
+    }
+  ];
+  return doc;
 }
