63e9c4f33cedacd66465868219a93b71c6143236
Nadee <nadee013@gmail.com>
2015-01-15 14:56:11 +0530

    added logger to pubs

--- server/publication.js
+++ server/publication.js
@@ -26,7 +26,7 @@ Meteor.publish("dailyShift", function(date) {
     var workersOnShifts = Workers.find({_id: {$in: workersList}});
     cursors.push(workersOnShifts);
   }
-  console.log("Daily shift publication");;
+  logger.info("Daily shift publication");;
   return cursors;
 });
 
@@ -39,6 +39,7 @@ Meteor.publish("weeklyShifts", function(dates) {
   var shiftsCursor = Shifts.find({"shiftDate": {$gte: firstDate, $lte: lastDate}});
   cursors.push(shiftsCursor);
 
+  //revenues on each date
   var revenueCursor = Revenue.find({"date": {$gte: firstDate, $lte: lastDate}});
   cursors.push(revenueCursor);
 
@@ -51,15 +52,19 @@ Meteor.publish("weeklyShifts", function(dates) {
     }
     shiftsList.push(shift._id);
   });
+
+  //jobs on each shift
   if(shiftsList.length > 0) {
     var jobsCursor = Jobs.find({"onshift": {$in: shiftsList}});
     cursors.push(jobsCursor);
   }
+
+  //workers on each shift
   if(workersList.length > 0) {
     var workersOnShifts = Workers.find({_id: {$in: workersList}});
     cursors.push(workersOnShifts);
   }
-  console.log("Weekly shifts publication");
+  logger.info("Weekly shifts publication");
   return cursors;
 });
 
@@ -67,15 +72,18 @@ Meteor.publish("jobsToBeCompleted", function() {
   var cursors = [];
   var jobs = Jobs.find({"status": "draft"});
   cursors.push(jobs);
+  logger.info("Jobs To Be Completed publication");
   return cursors;
 });
 
-Meteor.publish("availableWorkers", function(date) {
+Meteor.publish("activeWorkers", function(date) {
   var cursors = [];
   var busyWorkers = [];
   var onShiftWorkers = [];
   var onHolidayWorkers = [];
   var shifts = Shifts.find({'shiftDate': date}).fetch();
+
+  //workers on shifts
   if(shifts.length > 0) {
     shifts.forEach(function(shift) {
       if(shift.assignedTo) {
@@ -83,34 +91,45 @@ Meteor.publish("availableWorkers", function(date) {
       }
     });
   }
+
+  //workers on holiday
   var holidays = Holidays.findOne({"date": date});
   if(holidays) {
     if(holidays.users.length > 0) {
       onHolidayWorkers = holidays.users;
     }
   }
+  //all busy workers
   busyWorkers = onShiftWorkers.concat(onHolidayWorkers);
+  //active workers
   var workers = Workers.find({_id: {$nin: busyWorkers}, 'resign': false});
+  logger.info("ActiveWorkers publication");
   return workers;
 });
 
 Meteor.publish("admin", function() {
   var cursors = [];
+  //all workers
   cursors.push(Workers.find());
+  //all worker types
   cursors.push(WorkerTypes.find());
+  //all job types
   cursors.push(JobTypes.find());
+  logger.info("Admin publication");
   return cursors;
 });
 
 Meteor.publish('workerTypes', function() {
   var cursors = [];
   cursors.push(WorkerTypes.find());
+  logger.info("WorkerTypes publication");
   return cursors;
 });
 
 Meteor.publish('jobTypes', function() {
   var cursors = [];
   cursors.push(JobTypes.find());
+  logger.info("JobTypes publication");
   return cursors;
 });
 
