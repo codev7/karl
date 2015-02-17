b60c776aba8c18f2c961f6b7c6b250fb57b801c7
Nadee <nadee013@gmail.com>
2015-02-17 22:51:50 +0530

    added menu item detail view

--- client/views/menu/submit_menu_item.js
+++ client/views/menu/submit_menu_item.js
@@ -1,4 +1,4 @@
-Template.submitMenu.events({
+Template.submitMenuItem.events({
   'click .addjob': function(e, instance) {
     e.preventDefault();
     var htmlInput = "<input type='text' name='jobItems' class='form-control'/> <br/>"
