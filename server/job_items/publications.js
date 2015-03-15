52d9e9150679893e1d2f26fcdbdedba9f86493f1
Nadee <nadee013@gmail.com>
2015-03-16 01:43:33 +0530

    menu item subscription set

--- server/job_items/publications.js
+++ server/job_items/publications.js
@@ -8,3 +8,15 @@ Meteor.publish("jobItem", function(id) {
   cursors.push(JobItems.find(id));
   return cursors;
 });
+
+Meteor.publish("menuItemJobItems", function(ids) {
+  var cursors = [];
+  var items = null;
+  if(ids.length > 0) {
+    items = JobItems.find({"_id": {$in: ids}}, {sort: {'name': 1}, limit: 10});
+  } else {
+    items = JobItems.find({}, {sort: {'name': 1}, limit: 10});
+  }
+  cursors.push(items);
+  return cursors;
+});
