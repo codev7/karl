d97914a3757c343b54f032e84495b6bcd2b6a635
Nadee <nadee013@gmail.com>
2015-03-16 17:22:52 +0530

    fix ingredient search issue

--- client/lib/components/edit_job_item/view.js
+++ client/lib/components/edit_job_item/view.js
@@ -9,6 +9,7 @@ Template.editJobItem.helpers({
     var ing = Session.get("selectedIngredients");
     if(ing) {
       if(ing.length > 0) {
+        Meteor.subscribe("ingredients", ing);
         var ingredientsList = Ingredients.find({'_id': {$in: ing}}).fetch();
         return ingredientsList;
       }
--- server/ingredients/publications.js
+++ server/ingredients/publications.js
@@ -7,7 +7,7 @@ Meteor.publish("ingredients", function(ids) {
   var cursors = [];
   var ings = null;
   if(ids.length > 0) {
-    ings = Ingredients.find({"_id": {$in: ids}}, {sort: {'code': 1}, limit: 10});
+    ings = Ingredients.find({"_id": {$in: ids}}, {sort: {'code': 1}});
   } else {
     ings = Ingredients.find({}, {sort: {'code': 1}, limit: 10});
   }
@@ -15,21 +15,21 @@ Meteor.publish("ingredients", function(ids) {
   return cursors;
 });
 
-Meteor.publish("ingredientsSearch", function(searchText, notInIds) {
-  var ingredients = null;
-  var query = {};
-  var cursors = [];
-  var filter = null;
+// Meteor.publish("ingredientsSearch", function(searchText, notInIds) {
+//   var ingredients = null;
+//   var query = {};
+//   var cursors = [];
+//   var filter = null;
 
-  if(searchText) {
-    filter = new RegExp(searchText, 'i');
-  }
-  if(notInIds.length > 0) {
-    query["_id"] = {$nin: notInIds};
-  }
-  if(filter) {
-    query = {"code": filter};
-  }
-  cursors = Ingredients.find(query, {sort: {'code': 1}, limit: 10});
-  return cursors;
-});
+//   if(searchText) {
+//     filter = new RegExp(searchText, 'i');
+//   }
+//   if(notInIds.length > 0) {
+//     query["_id"] = {$nin: notInIds};
+//   }
+//   if(filter) {
+//     query = {"code": filter};
+//   }
+//   cursors = Ingredients.find(query, {sort: {'code': 1}, limit: 10});
+//   return cursors;
+// });
