5163dce815a9061216e5924337c635ce6146710c
Nadee <nadee013@gmail.com>
2015-03-17 08:59:26 +0530

    search with desc

--- server/ingredients/search.js
+++ server/ingredients/search.js
@@ -3,24 +3,15 @@ SearchSource.defineSource('ingredients', function(searchText, options) {
   var docs = [];
   if(searchText) {
     var regExp = buildRegExp(searchText);
-    var selector = {'code': regExp}
-    // var selector = {$or: [
-      // {'code': regExp}
-      // {'description': regExp}
-    // ]};
+    var selector = {$or: [
+      {'code': regExp},
+      {'description': regExp}
+    ]};
 
     docs = Ingredients.find(selector, options).fetch();
   } else {
     docs = Ingredients.find({}, options).fetch();
   }
-  // if(docs.length > 0) {
-  //   docs.forEach(function(item) {
-  //     var ing = getIngredientItem(item._id);
-  //     if(ing) {
-  //       item.costPerPortionUsed = ing.costPerPortionUsed;
-  //     }
-  //   });
-  // }
   return docs;
 });
 
