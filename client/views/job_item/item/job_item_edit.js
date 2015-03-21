9748e0ad4b150847f9d760af418346ae3bfcc870
Nadee <nadee013@gmail.com>
2015-03-21 12:36:01 +0530

    added menu item job remove icon and restructure components

--- client/views/job_item/item/job_item_edit.js
+++ client/views/job_item/item/job_item_edit.js
@@ -10,3 +10,22 @@ Template.jobItemEdit.helpers({
     }
   }
 });
+
+Template.jobItemEdit.events({
+  'click .removePrep': function(event) {
+    event.preventDefault();
+    var id = $(event.target).attr("data-id");
+    var jobItemsList = Session.get("selectedJobItems");
+    if(jobItemsList) {
+      if(jobItemsList.length > 0) {
+        var index = jobItemsList.indexOf(id);
+        if(index >= 0) {
+          jobItemsList.splice(index, 1);
+          Session.set("selectedJobItems", jobItemsList);
+        }
+      }
+    }
+    var item = $(event.target).parent().parent();
+    $(item).remove();
+  }
+});
