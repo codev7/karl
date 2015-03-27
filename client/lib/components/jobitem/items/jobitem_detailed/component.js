8137d343d54cf068240458ee51ff8e89cae88e72
Nadee <nadee013@gmail.com>
2015-03-27 16:34:55 +0530

    job item detailed component

---
+++ client/lib/components/jobitem/items/jobitem_detailed/component.js
@@ -0,0 +1,34 @@
+var component = FlowComponents.define('jobItemDetailed', function(props) {
+  this.jobitem = props.jobitem;
+  var item = getPrepItem(this.jobitem._id);
+  // console.log(item);
+  this.jobitem = item;
+});
+
+component.state.id = function() {
+  return this.jobitem._id;
+}
+
+component.state.name = function() {
+  return this.jobitem.name;
+}
+
+component.state.activeTime = function() {
+  return this.jobitem.activeTime;
+}
+
+component.state.type = function() {
+  return this.jobitem.type;
+}
+
+component.state.shelfLife = function() {
+  return this.jobitem.shelfLife;
+}
+
+component.state.portions = function() {
+  return this.jobitem.portions;
+}
+
+component.state.cost = function() {
+  return this.jobitem.prepCostPerPortion;
+}
--- client/views/job_item/item/job_item_detailed.js
+++ client/views/job_item/item/job_item_detailed.js
@@ -1,26 +1,26 @@
-Template.jobItemDetailed.helpers({
-  'cost': function() {
-    if(this) {
-      var id = this._id;
-      var item = getPrepItem(id);
-      if(item) {
-        return item.prepCostPerPortion;
-      }
-    }
-  }
-});
+// Template.jobItemDetailed.helpers({
+//   'cost': function() {
+//     if(this) {
+//       var id = this._id;
+//       var item = getPrepItem(id);
+//       if(item) {
+//         return item.prepCostPerPortion;
+//       }
+//     }
+//   }
+// });
 
-Template.jobItemDetailed.events({
-  'click .deleteJobItem': function(event) {
-    event.preventDefault();
-    var result = confirm("Are you sure, you want to delete this item ?");
-    if (result == true) {
-      Meteor.call("deleteJobItem", this._id, function(err) {
-        if(err) {
-          console.log(err);
-          return alert(err.reason);
-        }
-      });
-    }
-  }
-});
+// Template.jobItemDetailed.events({
+//   'click .deleteJobItem': function(event) {
+//     event.preventDefault();
+//     var result = confirm("Are you sure, you want to delete this item ?");
+//     if (result == true) {
+//       Meteor.call("deleteJobItem", this._id, function(err) {
+//         if(err) {
+//           console.log(err);
+//           return alert(err.reason);
+//         }
+//       });
+//     }
+//   }
+// });
