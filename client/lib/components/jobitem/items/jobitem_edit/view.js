2f2bf64f2777029c95d68dcf81d0cc18a33f169c
Nadee <nadee013@gmail.com>
2015-03-29 23:13:21 +0530

    added job item view component

--- client/lib/components/jobitem/items/jobitem_edit/view.js
+++ client/lib/components/jobitem/items/jobitem_edit/view.js
@@ -1,15 +1,15 @@
-Template.jobItemEdit.helpers({
-  item: function() {
-    if(this) {
-      if(this._id) {
-        var item = getPrepItem(this._id);
-        if(item) {
-          return item;
-        }
-      } 
-    }
-  }
-});
+// Template.jobItemEdit.helpers({
+//   item: function() {
+//     if(this) {
+//       if(this._id) {
+//         var item = getPrepItem(this._id);
+//         if(item) {
+//           return item;
+//         }
+//       } 
+//     }
+//   }
+// });
 
 Template.jobItemEdit.events({
   'click .removePrep': function(event) {
--- client/lib/components/jobitem/items/jobitem_view/component.js
+++ client/lib/components/jobitem/items/jobitem_view/component.js
@@ -1,4 +1,4 @@
-var component = FlowComponents.define('jobItemEdit', function(props) {
+var component = FlowComponents.define('jobItemView', function(props) {
   this.jobitem = props.jobitem;
   var item = getPrepItem(this.jobitem._id);
   this.jobitem = item;
@@ -13,14 +13,12 @@ component.state.name = function() {
   return this.jobitem.name;
 }
 
-component.state.type = function() {
-  return this.jobitem.type;
-}
-
-component.state.costPerPortion = function() {
-  return this.jobitem.prepCostPerPortion;
-}
-
 component.state.quantity = function() {
   return this.jobitem.quantity;
+}
+
+component.state.cost = function() {
+  var cost = this.jobitem.prepCostPerPortion * this.jobitem.quantity;
+  cost = Math.round(cost * 100)/100;
+  return cost;
 }
