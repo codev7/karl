3d0a01a1b47a37027164b291e86086745247bd0a
Nadee <nadee013@gmail.com>
2015-03-30 22:56:08 +0530

    added submit jobitem theme

--- client/lib/components/jobitem/items/jobitem_detailed/view.js
+++ client/lib/components/jobitem/items/jobitem_detailed/view.js
@@ -1,9 +1,10 @@
 Template.jobItemDetailed.events({
   'click .deleteJobItem': function(event) {
     event.preventDefault();
+    var id = $(event.target).attr("data-id");
     var result = confirm("Are you sure, you want to delete this item ?");
     if (result == true) {
-      Meteor.call("deleteJobItem", this._id, function(err) {
+      Meteor.call("deleteJobItem", id, function(err) {
         if(err) {
           console.log(err);
           return alert(err.reason);
