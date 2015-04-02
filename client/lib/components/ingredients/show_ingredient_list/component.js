06c5acafe6ea40778c22ed5cc52af2434a566e7d
Nadee <nadee013@gmail.com>
2015-04-02 16:28:02 +0530

    ing search remove existing ids

--- client/lib/components/ingredients/show_ingredient_list/component.js
+++ client/lib/components/ingredients/show_ingredient_list/component.js
@@ -1,2 +1,56 @@
 var component = FlowComponents.define('showIngredientsList', function(props) {
+  this.onRendered(this.renderShowIngList);
+  var id = Router.current().params._id;
+
+  var options = {
+    keepHistory: 1000 * 60 * 5,
+    localSearch: true
+  };
+  var fields = ['code', 'description'];
+
+  this.IngredientsSearch = new SearchSource('ingredients', fields, options);
+
+  if(props.name) {
+    if(props.name == "editJob") {
+      this.item = JobItems.findOne(id);
+    } else if(props.name == "editMenu") {
+      this.item = MenuItems.findOne(id);
+    }
+  }
 });
+
+component.prototype.setIds = function() {
+  var ids = [];
+  if(this.item) {
+    if(this.item.ingredients.length > 0) {
+      this.item.ingredients.forEach(function(doc) {
+        ids.push(doc._id);
+      });
+    }
+  }
+  this.set("ids", ids);
+  return ids;
+}
+
+component.prototype.renderShowIngList = function() {
+  var ids = this.setIds();
+  this.IngredientsSearch.search("", {"ids": ids});
+}
+
+component.state.getIngredients = function() {
+  return this.IngredientsSearch.getData({
+    transform: function(matchText, regExp) {
+      return matchText.replace(regExp, "<b>$&</b>")
+    },
+    sort: {'code': 1}
+  });
+}
+
+component.action.keyup = function(text) {
+  var ids = this.setIds();
+  this.IngredientsSearch.search(text, {"ids": ids});
+}
+
+component.action.submit = function() {
+  this.IngredientsSearch.search("");
+}
