252e065574bf255fda6c8e0ef26a6c08a1ef14e4
Nadee <nadee013@gmail.com>
2015-04-03 14:24:44 +0530

    load more button job items

--- client/lib/components/jobitem/jobitems_list/view.js
+++ client/lib/components/jobitem/jobitems_list/view.js
@@ -1,6 +1,11 @@
-Template.jobItemsList.helpers({
-  jobItemsList: function() {
-    var list = JobItems.find().fetch();
-    return list
-  }
+Template.jobItemsList.events({
+  'keyup #searchJobItemsBox': _.throttle(function(e) {
+    var text = $(e.target).val().trim();
+    FlowComponents.callAction('keyup', text);
+  }, 200),
+
+  'click #loadMoreJobItems': _.throttle(function(e) {
+    e.preventDefault();
+    FlowComponents.callAction('click');
+  }, 200)
 });
---
+++ server/job_items/search.js
@@ -0,0 +1,35 @@
+SearchSource.defineSource('jobItems', function(searchText, options) {
+  var optionFileds = {sort: {'name': 1}};
+  var docs = [];
+  var selector = {};
+  if(options) {
+    if(options.endingAt) {
+      selector['$or'] = [
+        {'name': {$gt: options.endingAt}},
+        {'name': {$lte: options.endingAt}}
+      ]
+    }
+    if(options.limit) {
+      optionFileds['limit'] = options.limit;
+    }
+    // if(options.ids) {
+    //   selector['_id'] = {$nin: options.ids}
+    // }
+  } else {
+    optionFileds['limit'] = 10;
+  }
+  if(searchText) {
+    var regExp = buildRegExp(searchText);
+    selector['$or'] = [
+      {'name': regExp}    
+    ];
+    docs = JobItems.find(selector, optionFileds).fetch();
+  } else {
+    docs = JobItems.find(selector, optionFileds).fetch();
+  }
+  return docs;
+});
+
+function buildRegExp(searchText) {
+  return new RegExp(searchText, 'i');
+}
