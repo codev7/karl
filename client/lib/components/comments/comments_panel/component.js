2d907f6470a2cd46fef93f577ebbca8f9e7f9939
Nadee <nadee013@gmail.com>
2015-04-07 15:38:54 +0530

    comment scroll top

--- client/lib/components/comments/comments_panel/component.js
+++ client/lib/components/comments/comments_panel/component.js
@@ -1,2 +1,12 @@
 var component = FlowComponents.define('commentsPanel', function(props) {
 });
+
+component.state.commentsExist = function() {
+  var item = Session.get("thisMenuItem");
+  var count = Comments.find({"reference": item}).count();
+  if(count > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
