c38ef0996116f9d52c69c0743bcb82b7d61e3c7d
Nadee <nadee013@gmail.com>
2015-04-07 15:53:42 +0530

    comment panel added to job items

--- client/lib/components/comments/comments_panel/component.js
+++ client/lib/components/comments/comments_panel/component.js
@@ -1,12 +1,19 @@
 var component = FlowComponents.define('commentsPanel', function(props) {
+  this.referenceId = Router.current().params._id;
 });
 
 component.state.commentsExist = function() {
-  var item = Session.get("thisMenuItem");
+  var item = this.referenceId;
   var count = Comments.find({"reference": item}).count();
   if(count > 0) {
     return true;
   } else {
     return false;
   }
+}
+
+component.state.commentsList = function() {
+  var item = this.referenceId;
+  var comments = Comments.find({"reference": item}).fetch();
+  return comments;
 }
--- client/lib/components/comments/submit_comment/view.js
+++ client/lib/components/comments/submit_comment/view.js
@@ -4,13 +4,7 @@ Template.submitComment.events({
       event.preventDefault();
       var text = $(".message-input").val();
       var ref = Session.get("thisMenuItem");
-      Meteor.call("createComment", text, ref, function(err, id) {
-        if(err) {
-          console.log(err);
-          return alert(err.reason);
-        }
-        $('.message-input').val("");
-      });
+      FlowComponents.callAction('submit', text);
     }
   }
 });
