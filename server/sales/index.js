3397af95274bb6ccd509d43d7b0cb34b7646f6b1
Nadee <nadee013@gmail.com>
2015-04-09 11:14:37 +0530

    added server side methods and aggregation framework

--- server/sales/index.js
+++ server/sales/index.js
@@ -1,18 +1,31 @@
 Meteor.methods({
-  'createSales': function(sales, date) {
-    if(!sales) {
-      logger.error("Sales field does not exist");
-      throw new Meteor.Error(404, "Sales field does not exist");
+  'createSalesMenus': function(date, menuId, quantity) {
+    if(!Meteor.userId()) {
+      logger.error('No user has logged in');
+      throw new Meteor.Error(401, "User not logged in");
+    }
+    var userId = Meteor.userId();
+    var permitted = isManagerOrAdmin(userId);
+    if(!permitted) {
+      logger.error("User not permitted to create ingredients");
+      throw new Meteor.Error(404, "User not permitted to create ingredients");
     }
     if(!date) {
       logger.error("Date field does not exist");
       throw new Meteor.Error(404, "Date field does not exist");
     }
+    var menuItem = MenuItems.findOne(menuId);
+    if(!menuItem) {
+      logger.error("Menu item does not exist");
+      throw new Meteor.Error(404, "Menu item does not exist");
+    }
     var doc = {
-      "sales": sales,
       "date": date,
+      "menuItem": menuId,
+      "quantity": quantity,
+      "soldAtPrice": menuItem.salesPrice,
       "createdOn": new Date(),
-      "createdBy": null
+      "createdBy": userId
     }
     var id = Sales.insert(doc);
     logger.info("New sales entry created", {"id": id});
@@ -20,24 +33,54 @@ Meteor.methods({
 
   },
 
-  'editSales': function(id, date, sales) {
+  'editSalesMenuQuantity': function(id, menuId, quantity) {
+    if(!Meteor.userId()) {
+      logger.error('No user has logged in');
+      throw new Meteor.Error(401, "User not logged in");
+    }
+    var userId = Meteor.userId();
+    var permitted = isManagerOrAdmin(userId);
+    if(!permitted) {
+      logger.error("User not permitted to create ingredients");
+      throw new Meteor.Error(404, "User not permitted to create ingredients");
+    }
+    if(!date) {
+      logger.error("Date field does not exist");
+      throw new Meteor.Error(404, "Date field does not exist");
+    }
+    var salesMenu = Sales.findOne({"_id": id, "menuItem": menuId});
+    if(!salesMenu) {
+      logger.error("Sales Menu does not exist");
+      throw new Meteor.Error(404, "Sales Menu does not exist");
+    }
+    var id = Sales.update({"_id": id}, {$set: {"quantity": quantity}});
+    logger.info("Sales entry updated", {"id": id});
+    return;
+
+  },
+
+  'deleteSalesMenu': function(id) {
+    if(!Meteor.userId()) {
+      logger.error('No user has logged in');
+      throw new Meteor.Error(401, "User not logged in");
+    }
+    var userId = Meteor.userId();
+    var permitted = isManagerOrAdmin(userId);
+    if(!permitted) {
+      logger.error("User not permitted to create ingredients");
+      throw new Meteor.Error(404, "User not permitted to create ingredients");
+    }
     if(!id) {
-      logger.error("Sales id field does not exist");
-      throw new Meteor.Error(404, "Sales id field does not exist");
+      logger.error('Id not found');
+      throw new Meteor.Error(401, "Id not found");
     }
-    var updateDoc = {};
-    if(sales) {
-      updateDoc.sales = sales;
+    var salesMenu = Sales.findOne(id);
+    if(!salesMenu) {
+      logger.error('Sales Menu does not exist');
+      throw new Meteor.Error(401, "Sales Menu does not exist");
     }
-    if(date) {
-      updateDoc.date = date;
-    }
-    if(Object.keys(updateDoc).length <= 0) {
-      logger.error("Sales has nothing to be updated");
-      throw new Meteor.Error(401, "Sales has nothing to be updated");
-    }
-    Sales.update({"_id": id}, {$set: updateDoc});
-    logger.info("Sales entry updated ", {"id": id});
+    Sales.remove({"_id": id});
+    logger.info("Sales entry deleted ", {"id": id});
     return;
   }
 });
--- server/sales/publications.js
+++ server/sales/publications.js
@@ -1,9 +1,24 @@
-Meteor.publish("weeklySales", function(firstDate, lastDate) {
+Meteor.publish("salesOnDate", function(date) {
   var cursors = [];
-  //Sales on a date range
-  var salesCursor = Sales.find({"date": {$gte: firstDate, $lte: lastDate}});
+  var salesCursor = Sales.find({"date": date});
   cursors.push(salesCursor);
-  
-  logger.info("Weekly sales publication");
   return cursors;
+});
+
+Meteor.publish("salesOnDateRange", function(range) {
+  var todayInMiliSecs = new Date().getTime();
+  var dateRange = parseInt(range) * 24 * 60 * 60 * 1000;
+  var calcMiliSecs = todayInMiliSecs + dateRange;
+
+
+  var historyDate = new Date(calcMiliSecs).toJSON().slice(0, 10);
+  var today = new Date().toJSON().slice(0, 10);
+  
+  console.log("........historyDate.....", historyDate, today);
+
+  var pipeline = [
+    {$group: {"menu": "$menuItem", "soldAtPrice": "$soldAtPrice", "totalQty": {$sum: "$quantity"}}}
+  ]
+  var sales = Sales.aggregate(pipeline);
+  return sales;
 });
