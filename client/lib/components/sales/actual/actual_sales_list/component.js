61da8b5bdde446e98105d57dfcd7f12bd14ea14c
Nadee <nadee013@gmail.com>
2015-04-12 11:18:16 +0530

    salea total count cache issue fiz

--- client/lib/components/sales/actual/actual_sales_list/component.js
+++ client/lib/components/sales/actual/actual_sales_list/component.js
@@ -2,22 +2,3 @@ var component = FlowComponents.define("actualSalesList", function(props) {
   var date = Router.current().params.date;
   this.set("date", date);
 });
-
-component.state.totalItemCount = function() {
-  var total = 0;
-  var sales = Sales.find({"date": new Date(this.get("date"))}).fetch();
-  sales.forEach(function(doc) {
-    total += doc.quantity;
-  });
-  return total;
-}
-
-component.state.totalRevenue = function() {
-  var total = 0;
-  var sales = Sales.find({"date": new Date(this.get("date"))}).fetch();
-  sales.forEach(function(doc) {
-    var revenue = doc.quantity * doc.soldAtPrice;
-    total += revenue;
-  });
-  return total;
-}
--- client/lib/components/sales/sales_list/component.js
+++ client/lib/components/sales/sales_list/component.js
@@ -48,3 +48,22 @@ component.prototype.renderedListOnRange = function() {
     }
   });
 }
+
+component.state.totalItemCount = function() {
+  var total = 0;
+  var sales = Sales.find({"date": new Date(this.get("date"))}).fetch();
+  sales.forEach(function(doc) {
+    total += doc.quantity;
+  });
+  return total;
+}
+
+component.state.totalRevenue = function() {
+  var total = 0;
+  var sales = Sales.find({"date": new Date(this.get("date"))}).fetch();
+  sales.forEach(function(doc) {
+    var revenue = doc.quantity * doc.soldAtPrice;
+    total += revenue;
+  });
+  return total;
+}
