45c457f5f50a0760e43162d9b10a34fb5d4a1f1a
Nadee <nadee013@gmail.com>
2015-04-21 14:46:00 +0530

    fixed generated jobs

---
+++ client/lib/components/jobs/items/item_listed/component.js
@@ -0,0 +1,33 @@
+var component = FlowComponents.define("jobListed", function(props) {
+  this.job = props.job;
+});
+
+component.state.name = function() {
+  return this.job.name;
+}
+
+component.state.type = function() {
+  return this.job.type;
+}
+
+component.state.status = function() {
+  return this.job.status;
+}
+
+component.state.activeTime = function() {
+  var time = this.job.activeTime;
+  time = parseInt(time/60);
+  return Math.round(time);;
+}
+
+component.state.createdOn = function() {
+  return this.job.createdOn;
+}
+
+component.state.shelfLife = function() {
+  return this.job.shelfLife;
+}
+
+component.state.portions = function() {
+  return this.job.portions;
+}
