240019e634c635b275855cc0de2bf83022aee6aa
Nadee <nadee013@gmail.com>
2015-04-21 14:25:05 +0530

    sales bug fix

--- client/lib/components/sales/actual/actual_sales_list/component.js
+++ client/lib/components/sales/actual/actual_sales_list/component.js
@@ -1,28 +1,4 @@
-var subs = new SubsManager();
-
 var component = FlowComponents.define("actualSalesList", function(props) {
   var date = Router.current().params.date;
   this.set("date", date);
-  subs.subscribe("salesOnDate", new Date(date));
 });
-
-component.state.salesMenusList = function() {
-  var sales = null;
-  var date = this.get("date");
-  if(date) {
-    sales = Sales.find({"date": new Date(date)});
-  } 
-  return sales;
-}
-
-component.state.totals = function() {
-  var totalQuantity = 0;
-  var totalRevenue = 0;
-  var sales = Sales.find({"date": new Date(this.get("date"))}).fetch();
-  sales.forEach(function(doc) {
-    totalQuantity += doc.quantity;
-    var revenue = doc.quantity * doc.soldAtPrice;
-    totalRevenue += revenue;
-  });
-  return {"totalItemCount": totalQuantity, "totalRevenue": totalRevenue};
-}
