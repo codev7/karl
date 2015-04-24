24cc6d94f0900d702c881aa6a3edfb2c84bd38be
Nadee <nadee013@gmail.com>
2015-04-24 17:30:58 +0530

    job delete added

--- client/lib/components/jobs/items/item_listed/view.js
+++ client/lib/components/jobs/items/item_listed/view.js
@@ -2,6 +2,14 @@ Template.jobListed.events({
   'click .deleteJob': function(event) {
     event.preventDefault();
     var id = $(event.target).attr("data-id");
-    console.log("........", id);
+    var confirmDelete = confirm("Are you sure you want to delete this job ?");
+    if(confirmDelete) {
+      Meteor.call("deleteJob", id, function(err) {
+        if(err) {
+          console.log(err);
+          return alert(err.reason);
+        }
+      });
+    }
   }
 });
