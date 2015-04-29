9efae284df5691cfbeee0bf56746aa3c5c042dc7
Nadee <nadee013@gmail.com>
2015-04-29 12:59:01 +0530

    shift date added

--- client/lib/components/scheduling/daily/component.js
+++ client/lib/components/scheduling/daily/component.js
@@ -1,3 +1,8 @@
 var component = FlowComponents.define("dailyShiftScheduling", function(props) {
   
 });
+
+component.state.date = function() {
+  var date = Router.current().params.date;
+  return moment(date).format("YYYY-MM-DD");
+}
