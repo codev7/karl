aa1b500fc6eb436f9a2f99d56f6aacfebfba4fa3
Nadee <nadee013@gmail.com>
2015-05-25 10:19:25 +0530

    added intercom

--- client/views/main.js
+++ client/views/main.js
@@ -2,4 +2,17 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault("counter", 0);
   Meteor.subscribe('currentUser');
+
+  IntercomSettings.userInfo = function(user, info) {
+    console.log(user, info);
+    if(Meteor.user().intercomHash) {
+      // add properties to the info object, for instance:
+      if(user.services && user.services.google) {
+        info.email = user.services.google.email;
+        info['Name'] = user.services.google.given_name + ' ' + user.services.google.family_name;
+      } else {
+        info.email = user.emails[0].address;
+      }
+    }
+  }
 }
