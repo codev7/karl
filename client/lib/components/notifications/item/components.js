a5b98c0e8f71c35b4f2ddbbc464efdc475fb2fff
Nadee <nadee013@gmail.com>
2015-05-25 12:12:19 +0530

    fixing settings bug

--- client/lib/components/notifications/item/components.js
+++ client/lib/components/notifications/item/components.js
@@ -73,7 +73,6 @@ component.state.createAt = function() {
 
 component.state.text = function() {
   if(this.notification && this.notification.text) {
-    console.log(this.notification.text);
     var text = this.notification.text;
     return text;
   }
