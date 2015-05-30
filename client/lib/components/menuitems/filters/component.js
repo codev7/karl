842af64a325443f9ba0b4b5be8c4a6e88188d3b7
Nadee <nadee013@gmail.com>
2015-05-30 20:24:20 +0530

    fixed filter

--- client/lib/components/menuitems/filters/component.js
+++ client/lib/components/menuitems/filters/component.js
@@ -2,13 +2,45 @@ var component = FlowComponents.define('menuFilters', function(props) {
 });
 
 component.state.categories = function() {
-  return Categories.find().fetch();
+  var selected = Session.get("category");
+  if(selected != "all") {
+    var categories = Categories.find({"_id": {$nin: [selected]}}).fetch();
+    categories.push({"name": "All", "_id": "all"});
+    return categories;
+  } else {
+    return Categories.find().fetch();
+  }
 }
 
 component.state.statuses = function() {
-  return Statuses.find().fetch();
+  var selected = Session.get("status");
+  if(selected != "all") {
+    var statuses = Statuses.find({"name": {$nin: [selected]}}).fetch();
+    statuses.push({"name": "All", "_id": "all"});
+    return statuses;
+  } else {
+    return Statuses.find().fetch();
+  }
 }
 
 component.state.isAdmin = function() {
   return isAdmin();
+}
+
+component.state.selectedCategory = function() {
+  var selected = Session.get("category");
+  if(selected != "all") {
+    return Categories.findOne(selected);
+  } else {
+    return {"name": "All", "_id": "all"};
+  }
+}
+
+component.state.selectedStatus = function() {
+  var selected = Session.get("status");
+  if(selected != "all") {
+    return Statuses.findOne({"name": selected});
+  } else {
+    return {"name": "All", "_id": "all"};
+  }
 }
