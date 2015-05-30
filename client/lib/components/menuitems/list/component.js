f8b61f1c975291b37142ee2f59922366ab2398e7
Nadee <nadee013@gmail.com>
2015-05-30 22:28:27 +0530

    remove sub from component to add fast render

--- client/lib/components/menuitems/list/component.js
+++ client/lib/components/menuitems/list/component.js
@@ -1,15 +1,9 @@
-var subs = new SubsManager();
-
 var component = FlowComponents.define('menuItemsList', function(props) {
-  var params = Router.current().params;
-  this.set("category", params.category);
-  this.set("status", params.status);
-  subs.subscribe("menuList", params.category, params.status);
 });
 
 component.state.list = function() {
-  var category = this.get("category");
-  var status = this.get("status");
+  var category = Session.get("category")
+  var status =  Session.get("status")
   if(category == "all" && status == "all") {
     return MenuItems.find();
   } else {
