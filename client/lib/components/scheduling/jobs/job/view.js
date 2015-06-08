c914f59cd8925fe7d7b824005a80500375a2a7a6
Nadee <nadee013@gmail.com>
2015-06-08 15:48:31 +0530

    added draggable for newly added job

--- client/lib/components/scheduling/jobs/job/view.js
+++ client/lib/components/scheduling/jobs/job/view.js
@@ -9,3 +9,20 @@ Template.schedulingJob.events({
     }); 
   }
 });
+
+Template.schedulingJob.rendered = function() {
+  $('#external-events div.external-event').each(function() {
+    // store data so the calendar knows to render an event upon drop
+    $(this).data('event', {
+      title: $.trim($(this).text()), // use the element's text as the event title,
+      id: $(this).attr("data-id"),
+      stick: true // maintain when user navigates (see docs on the renderEvent method)
+    });
+    // make the event draggable using jQuery UI
+    $(this).draggable({
+      zIndex: 1111999,
+      revert: true,      // will cause the event to go back to its
+      revertDuration: 0  //  original position after the drag
+    });
+  });
+}
