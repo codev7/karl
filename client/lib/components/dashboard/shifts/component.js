1e81fca3756083464da99af2ba6c77bdfe7cfbb4
Nadee <nadee013@gmail.com>
2015-06-10 12:10:59 +0530

    shifts confirm reject removed

--- client/lib/components/dashboard/shifts/component.js
+++ client/lib/components/dashboard/shifts/component.js
@@ -15,9 +15,13 @@ component.state.shifts = function() {
   var state = Session.get("shiftState");
   var shifts = [];
   if(state) {
-    shifts = Shifts.find({"assignedTo": Meteor.userId(), "shiftDate": {$gte: Date.now()}});
+    shifts = Shifts.find({"assignedTo": Meteor.userId(), "shiftDate": {$gte: Date.now()}}, {sort: {'shiftDate': 1}});
   } else {
-    shifts = Shifts.find({"assignedTo": Meteor.userId(), "shiftDate": {$lt: Date.now()}});
+    shifts = Shifts.find({"assignedTo": Meteor.userId(), "shiftDate": {$lt: Date.now()}}, {sort: {'shiftDate': -1}});
   }
   return shifts;
+}
+
+component.state.future = function() {
+  return Session.get("shiftState");
 }
