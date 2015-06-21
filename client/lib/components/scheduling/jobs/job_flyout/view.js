8e51a589d2edeb4c3bd32e625f7053998abb2bb4
Nadee <nadee013@gmail.com>
2015-06-21 16:27:43 +0530

    fixed dive overlay issue

--- client/lib/components/scheduling/jobs/job_flyout/view.js
+++ client/lib/components/scheduling/jobs/job_flyout/view.js
@@ -7,11 +7,19 @@ Template.jobFlyout.events({
         console.log(err);
       }
       $(".theme-config-box").toggleClass("show");
+      setTimeout(function() {
+        $(".flyout-container").css("z-index", -1);
+      }, 1000);
     });
   },
+
   'click .theme-config-close-btn': function(evt) {
     $(".theme-config-box").removeClass("show");
+    setTimeout(function() {
+      $(".flyout-container").css("z-index", -1);
+    }, 1000);
   },
+
   'click .checklist-check input': function(event) {
    var checked = $(event.target).is(":checked");
    checked ? $(event.target).closest('.checklist-check').addClass('checked') : $(event.target).closest('.checklist-check').removeClass('checked')
