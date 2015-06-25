41a4f1073f42097563d648c1ac32d4a856fb0657
Nadee <nadee013@gmail.com>
2015-06-25 12:00:57 +0530

    added permission for user

--- client/lib/components/roster/weekly/week/component.js
+++ client/lib/components/roster/weekly/week/component.js
@@ -20,39 +20,45 @@ component.state.origin = function() {
 }
 
 component.prototype.onListRendered = function() {
-  var origin = this.name;
-  $(".sortable-list").sortable({
-    "connectWith": ".sortable-list",
-    "revert": true
-  });
+  var user = Meteor.user();
+  if(user.isAdmin || Meteor.isManager) {
+    $(".sortable-list > div > li").css("cursor", "move");
+    var origin = this.name;
+    $(".sortable-list").sortable({
+      "connectWith": ".sortable-list",
+      "revert": true
+    });
 
 
-  $(".sortable-list").on("sortreceive", function(event, ui) {
-    var self = this;
-    var id = $(ui.item[0]).attr("data-id");//shiftid
-    var  newDate = $(this).attr("data-date")//date of moved list
-    if(origin == "weeklyrostertemplate") {
-      var daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-      newDate = parseInt(daysOfWeek.indexOf(newDate));
-    }
-    if(id && newDate) {
-      if(origin == "weeklyroster") {
-        Meteor.call("editShift", {"_id": id, "shiftDate": newDate}, function(err) {
-          if(err) {
-            console.log(err);
-            $(ui.sender[0]).sortable('cancel');;
-            return alert(err.reason);
-          }
-        });
-      } else if(origin == "weeklyrostertemplate") {
-        Meteor.call("editTemplateShift", {"_id": id, "shiftDate": newDate}, function(err) {
-          if(err) {
-            console.log(err);
-            $(ui.sender[0]).sortable('cancel');;
-            return alert(err.reason);
-          }
-        });
+    $(".sortable-list").on("sortreceive", function(event, ui) {
+      var self = this;
+      var id = $(ui.item[0]).attr("data-id");//shiftid
+      var  newDate = $(this).attr("data-date")//date of moved list
+      if(origin == "weeklyrostertemplate") {
+        var daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+        newDate = parseInt(daysOfWeek.indexOf(newDate));
       }
-    }
-  });
+      if(id && newDate) {
+        if(origin == "weeklyroster") {
+          Meteor.call("editShift", {"_id": id, "shiftDate": newDate}, function(err) {
+            if(err) {
+              console.log(err);
+              $(ui.sender[0]).sortable('cancel');;
+              return alert(err.reason);
+            }
+          });
+        } else if(origin == "weeklyrostertemplate") {
+          Meteor.call("editTemplateShift", {"_id": id, "shiftDate": newDate}, function(err) {
+            if(err) {
+              console.log(err);
+              $(ui.sender[0]).sortable('cancel');;
+              return alert(err.reason);
+            }
+          });
+        }
+      }
+    });
+  } else {
+    $(".sortable-list > div > li").css("cursor", "default");
+  }
 }
