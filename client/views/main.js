804c25884ff900ef298732cde5c285306c9f9fc6
Nadee <nadee013@gmail.com>
2015-06-30 08:51:39 +0530

    added user type-intercom(bug)

--- client/views/main.js
+++ client/views/main.js
@@ -1,7 +1,7 @@
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault("counter", 0);
-  Meteor.subscribe('currentUser');
+  Meteor.subscribe('profileUser', Meteor.userId());
 
   IntercomSettings.userInfo = function(user, info) {
     if(user.intercomHash) {
@@ -11,7 +11,20 @@ if (Meteor.isClient) {
         info['Name'] = user.services.google.given_name + ' ' + user.services.google.family_name;
       } else {
         info.email = user.emails[0].address;
+        info['Name'] = user.username;
       }
+      var type = "Worker";
+      if(user) {
+        if(user.isWorker) {
+          type = "Worker";
+        } else if(user.isManager) {
+          type = "Manager";
+        } else if(user.isAdmin) {
+          type = "Admin";
+        }
+      }
+      info['Type'] = type;
+
     } else {
       return false;
     }
