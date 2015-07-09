5ecbfd7f39785820eb56888c78ea14f28eea5266
Nadee <nadee013@gmail.com>
2015-07-09 16:14:02 +0530

    fixed goto notification bug

--- client/lib/components/dashboard/notifications/view.js
+++ client/lib/components/dashboard/notifications/view.js
@@ -12,29 +12,5 @@ Template.notificationsList.events({
     Session.set("notifiState", true);
     $(event.target).addClass("label-primary");
     $(".newNoti").removeClass("label-primary");
-  },
-
-  'click .confirmClaim': function(event) {
-    event.preventDefault();
-    var user = $(event.target).attr("data-id");
-    var shift = $(event.target).attr("data-shift");
-    Meteor.call("confirmClaim", shift, user, function(err) {
-      if(err) {
-        console.log(err);
-        return alert(err.reason);
-      } else {
-        var text = "Shift claim on " + moment(shift.shiftDate).format("ddd, Do MMMM") + " has been confirmed";
-        var options = {
-          "title": text,
-          "type": "confirm"
-        };
-        Meteor.call("sendNotifications", shift, "roster", options, function(err) {
-          if(err) {
-            console.log(err);
-            return alert(err.reason);
-          } 
-        });
-      }
-    });
   }
 });
