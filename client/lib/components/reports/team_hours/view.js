779f96a00df8b5baf09ced6cfa900f9d713fa7c9
karl <karlclauson@mail.com>
2015-07-28 15:27:41 +0200

    'Show all' button change to 'hide empty'

--- client/lib/components/reports/team_hours/view.js
+++ client/lib/components/reports/team_hours/view.js
@@ -1,11 +1,13 @@
 Template.teamHours.events({
   'click .shiftView': function(event) {
+    document.getElementById("showall_hideempty").innerText="Show All";
     var week = Router.current().params.week;
     Session.set("reportHash", "shifts");
     Router.go("teamHours", {"week": week}, {hash: 'shifts'});
   },
 
   'click .hoursView': function(event) {
+    document.getElementById("showall_hideempty").innerText="Show All";
     var week = Router.current().params.week;
     Session.set("reportHash", "hours");
     Router.go("teamHours", {"week": week}, {hash: 'hours'});
@@ -13,13 +15,23 @@ Template.teamHours.events({
   'click .showallView': function(event) {
     var week = Router.current().params.week;
     var hash = Session.get("reportHash");
-    if((hash=="shifts")||(hash=="shiftsall")) {
+    if(hash=="shifts") {
+      document.getElementById("showall_hideempty").innerHTML="Hide Empty";
       Session.set("reportHash", "shiftsall");
       Router.go("teamHours", {"week": week}, {hash: 'shiftsall'});
     }
-    else if((hash=="hours")||(hash=="hoursall")) {
+    else if(hash=="hours") {
+      document.getElementById("showall_hideempty").innerText="Hide Empty";
       Session.set("reportHash", "hoursall");
       Router.go("teamHours", {"week": week}, {hash: 'hoursall'});
+    }else if(hash=="shiftsall") {
+      document.getElementById("showall_hideempty").innerText="Show All";
+      Session.set("reportHash", "shifts");
+      Router.go("teamHours", {"week": week}, {hash: 'shifts'});
+    }else if(hash=="hoursall") {
+      document.getElementById("showall_hideempty").innerText="Show All";
+      Session.set("reportHash", "hours");
+      Router.go("teamHours", {"week": week}, {hash: 'hours'});
     }
   }
 });
